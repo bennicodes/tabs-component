88e19fdf0a1ca66242460e741377b17fd2b1099a
Benedict Notoane <benni@Benedicts-MacBook-Pro.local>
2024-11-20 11:03:23 +0100

    project complete

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,18 @@
 const tabTitleButtons = document.querySelectorAll(".tabs__title");
 const tabContents = document.querySelectorAll(".tabs__content");
+
+tabTitleButtons.forEach((tabButton, index) => {
+  tabButton.addEventListener("click", (e) => {
+    tabContents.forEach((content) =>
+      content.classList.remove("tabs__content--active")
+    );
+
+    tabTitleButtons.forEach((button) =>
+      button.classList.remove("tabs__title--active")
+    );
+
+    e.target.classList.add("tabs__title--active");
+
+    tabContents[index].classList.add("tabs__content--active");
+  });
+});
